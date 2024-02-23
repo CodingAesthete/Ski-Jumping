e4a05c905cdce3a0c60cb352f5eef77be299afbd
CodingAesthete <harry_olli@abv.bg>
2024-02-23 08:58:10 +0200

    Adjusting the Positioning of the Error Message on the Login Page

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -76,7 +76,7 @@ export default function SignIn() {
           <span className='text-red-700'>Sign up</span>
         </Link>
       </div>
-      {error && <p className='bg-blue-50 bg-opacity-90 text-red-600'>{error}</p>}
+      {error && <p className='px-3 my-2 font-bold bg-blue-50 bg-opacity-90 text-red-600'>{error}</p>}
     </div>
   )
 
