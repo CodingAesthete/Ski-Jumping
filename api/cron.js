fb7be4879eada351c5758dfb8780fc46d62f60f3
CodingAesthete <harry_olli@abv.bg>
2024-02-25 22:12:57 +0200

    correct backendendpoint

--- api/cron.js
+++ api/cron.js
@@ -1,7 +1,7 @@
 import cron from 'cron';
 import https from 'https';
 
-const backendUrl = '/api/user/test'; // Specify your backend URL here
+const backendUrl = 'http://localhost:3000/api/user/test'; // Specify your backend URL here
 
 const job = new cron.CronJob('*/14 * * * *', function () {
   console.log('Restarting server');
